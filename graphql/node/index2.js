66253da007732024eadefefa53897011c00a3817
Elliot Wright <wright.elliot@gmail.com>
2018-03-22 10:42:49 +0000

    Updated benchs.

--- graphql/node/index2.js
+++ graphql/node/index2.js
@@ -4,7 +4,7 @@ const hrtime = process.hrtime;
 
 let hrstart = hrtime();
 
-for (let i = 0; i < 1000000; i++) {
+for (let i = 0; i < 5000000; i++) {
     const lexer = gqllanguage.createLexer(new gqllanguage.Source("query foo { name model }"), {});
 
     do {
@@ -14,18 +14,5 @@ for (let i = 0; i < 1000000; i++) {
 }
 
 let hrend = hrtime(hrstart);
-
-console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1]/1000000);
-
-hrstart = hrtime();
-
-const lexer = gqllanguage.createLexer(new gqllanguage.Source("query foo { name model }"), {});
-
-do {
-    let foo = lexer.token;
-    lexer.advance();
-} while(lexer.token.kind !== gqllexer.TokenKind.EOF);
-
-hrend = hrtime(hrstart);
 
 console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1]/1000000);
