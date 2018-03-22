ed63f0045135d5fdde6c8eec87efd9db5eec0289
Elliot Wright <wright.elliot@gmail.com>
2018-03-22 15:46:03 +0000

    Reader tests.

---
+++ reader/rd4m_test.go
@@ -0,0 +1,45 @@
+package reader
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+func BenchmarkReading4MFile(b *testing.B) {
+	var bs []byte
+
+	for i := 0; i < b.N; i++ {
+		// Generate this file with: `$ dd if=/dev/urandom of=/tmp/4m bs=1K count=4096`
+		file, err := os.Open("/tmp/4m")
+		if err != nil {
+			b.Error(err)
+		}
+
+		bs, err = ioutil.ReadAll(io.LimitReader(file, 2194000))
+		if err != nil {
+			b.Error(err)
+		}
+	}
+
+	b.Log(string(bs))
+}
+
+func BenchmarkReadingString(b *testing.B) {
+	var bs []byte
+	var err error
+
+	input := "Hello, 世界"
+	for i := 0; i < b.N; i++ {
+		rdr := strings.NewReader(input)
+
+		bs, err = ioutil.ReadAll(rdr)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+
+	_ = bs
+}
