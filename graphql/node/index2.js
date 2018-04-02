eeb77b4bbd7e0fd506a0484d7e236a4190bc9118
Elliot Wright <wright.elliot@gmail.com>
2018-04-02 15:10:45 +0100

    Matching Go benchmarks.

--- graphql/node/index2.js
+++ graphql/node/index2.js
@@ -5,7 +5,7 @@ const hrtime = process.hrtime;
 let hrstart = hrtime();
 
 for (let i = 0; i < 5000000; i++) {
-    const lexer = gqllanguage.createLexer(new gqllanguage.Source("query foo { name model }"), {});
+    const lexer = gqllanguage.createLexer(new gqllanguage.Source("query 0.001 foo { name model foo bar baz qux }"), {});
 
     do {
         let foo = lexer.token;
